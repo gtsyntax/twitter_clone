cd43ed744dacd26af156f863911fed59a0b60dc6
Greattony Oduche <greattony96@gmail.com>
2018-04-02 23:43:48 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const hbs = require('hbs');
+const expressHbs = require('express-handlebars');
+const port = process.env.PORT || 3030;
+
+const app = express();
+
+// app middlewares
+app.engine('.hbs', expressHbs({ defaultLayout: 'layout', extname: '.hbs'}));
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.listen(port, (err) => {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log("App listening on port 3030...");
+	}
+});
