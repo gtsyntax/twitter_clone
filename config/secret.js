a3cb2b12337ce5fd4c7cc12b7f483eecb91624fb
Greattony Oduche <greattony96@gmail.com>
2018-04-03 11:12:30 +0300

    Session and flash

--- config/secret.js
+++ config/secret.js
@@ -1,3 +1,4 @@
 module.exports = {
-	database: 'mongodb://root:abc123@ds231559.mlab.com:31559/twitterdb'
+	database: 'mongodb://root:abc123@ds231559.mlab.com:31559/twitterdb',
+	secret: 'thisisasecret'
 }
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const hbs = require('hbs');
 const expressHbs = require('express-handlebars');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const flash = require('express-flash');
 const config = require('./config/secret');
 const port = process.env.PORT || 3030;
 
@@ -23,6 +26,13 @@ app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
+app.use(session({
+	resave: true,
+	saveUninitialized: true,
+	secret: config.secret,
+	store: new MongoStore({ url: config.database, autoReconnect: true })
+}));
+app.use(flash());
 
 
 // app route
